9163920fc1f4bf26f361295ad84fce86d4afc208
srikanthpragada <srikanthpragada@gmail.com>
2026-09-09 20:55:02 +0530

    09-SEP

--- llms/lc/6.stream.py
+++ llms/lc/6.stream.py
@@ -1,8 +1,12 @@
 # Set environment variable GOOGLE_API_KEY to Google key.
 
 from langchain.chat_models import init_chat_model
+from google.genai.types import AutomaticFunctionCallingConfig
 
 model = init_chat_model("gemini-2.5-flash", model_provider="google_genai")
+# To supress warning from google SDK
+model = model.bind(automatic_function_calling=AutomaticFunctionCallingConfig(disable=True))
+
 #response = model.invoke("Who is James Gosling")
 response = model.stream("Who is Van Rossum?")
 
